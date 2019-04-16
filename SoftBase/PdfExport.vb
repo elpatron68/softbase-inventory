77ea0e09135bdd115763200948aaa9209cd77616
elpatron68 <elpatron@cepheus.uberspace.de>
2019-04-16 23:29:09 +0200

    PDF title, font

--- SoftBase/PdfExport.vb
+++ SoftBase/PdfExport.vb
@@ -13,13 +13,13 @@ Public Class PdfExport
         Dim writer As PdfWriter = New PdfWriter(dest)
         Dim pdf As PdfDocument = New PdfDocument(writer)
         Dim document As Document = New Document(pdf)
-        Dim font As PdfFont = PdfFontFactory.CreateFont(StandardFonts.TIMES_ROMAN)
-        document.Add(New Paragraph("iText is:").SetFont(font))
+        Dim font As PdfFont = PdfFontFactory.CreateFont(StandardFonts.TIMES_BOLD)
+        document.Add(New Paragraph("Software list for machine [NAME] created at [DATE]:").SetFont(font))
+        PdfFontFactory.CreateFont(StandardFonts.COURIER)
         Dim list As List = New List().SetSymbolIndent(12).SetListSymbol("•").SetFont(font)
         For Each soft In softlist
             list.Add($"{soft.Name} - Version {soft.Version}")
         Next
-        'list.Add(New ListItem("Never gonna give you up")).Add(New ListItem("Never gonna let you down")).Add(New ListItem("Never gonna run around and desert you")).Add(New ListItem("Never gonna make you cry")).Add(New ListItem("Never gonna say goodbye")).Add(New ListItem("Never gonna tell a lie and hurt you"))
         document.Add(list)
         document.Close()
     End Sub
