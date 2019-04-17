06c0af4f514de46bd4a69b217824d43af9096edc
elpatron68 <elpatron@cepheus.uberspace.de>
2019-04-17 23:55:03 +0200

    Settings, README

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# SoftBase
+# SoftBase Inventory
 
 [![Build status](https://elpatron68.visualstudio.com/SoftBase/_apis/build/status/SoftBase-.NET%20Desktop-CI%20(Auto))](https://elpatron68.visualstudio.com/SoftBase/_build/latest?definitionId=-1)
 
-SoftBase is a simple tool to inventory installed programs of multiple Windows devices.
+SoftBase is a simple tool to inventory installed programs of one or multiple Windows devices.
 
 ## Features
 
@@ -11,3 +11,14 @@ SoftBase is a simple tool to inventory installed programs of multiple Windows de
 - Export reports as PDF or Excel
 - Keep inventories of multiple devices
 - No limitaions, no nag screens, Free Open Source Software (FOSS)
+- No analytics, no tracking, maximum privacy
+
+## Requirements
+
+## Download
+
+## Installation
+
+## Usage
+
+## License
--- SoftBase/Settings.xaml.vb
+++ SoftBase/Settings.xaml.vb
@@ -1,3 +1,26 @@
-﻿Public Class Settings
+﻿Imports Microsoft.Win32
 
+Public Class Settings
+    Private Sub MetroWindow_Loaded(sender As Object, e As RoutedEventArgs)
+        TxDatabasepath.Text = My.Settings.databasefile
+    End Sub
+
+    Private Sub MetroWindow_Closed(sender As Object, e As EventArgs)
+        My.Settings.databasefile = TxDatabasepath.Text
+    End Sub
+
+    Private Sub BtnBrowse_Click(sender As Object, e As RoutedEventArgs) Handles BtnBrowse.Click
+        Dim Ofd As New OpenFileDialog()
+        With Ofd
+            .InitialDirectory = Environment.SpecialFolder.MyDocuments
+            .FileName = "SoftBase-Inventory.sqlite"
+            .CheckFileExists = False
+            .CheckPathExists = True
+            .Filter = "Sqlite files (*.sqlite)|*.sqlite|All files (*.*)|*.*"
+        End With
+        If Ofd.ShowDialog() = True Then
+
+        End If
+
+    End Sub
 End Class
